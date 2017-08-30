f62f4d88701d9e385247d3cded9aac8fe5c00034
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-08-30 14:47:38 +0200

    fix init for py3

--- pyinn/__init__.py
+++ pyinn/__init__.py
@@ -1,5 +1,5 @@
-from ncrelu import ncrelu
-from dgmm import dgmm
-from cdgmm import cdgmm
-from im2col import im2col, col2im
-from conv2d_depthwise import conv2d_depthwise
+from .ncrelu import ncrelu
+from .dgmm import dgmm
+from .cdgmm import cdgmm
+from .im2col import im2col, col2im
+from .conv2d_depthwise import conv2d_depthwise
