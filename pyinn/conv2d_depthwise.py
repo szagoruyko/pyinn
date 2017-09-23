c49af2d5796ddb56dce8b6b16a80fd62f01e6001
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-09-23 23:09:36 +0200

    tf kernel for backward wrt weight

--- pyinn/conv2d_depthwise.py
+++ pyinn/conv2d_depthwise.py
@@ -34,7 +34,8 @@ def GET_BLOCKS(N):
 
 _conv2d_depthwise_kernel = kernel_loop + '''
 extern "C"
-__global__ void conv2d_dw_forward_kernel(const ${Dtype}* input, const ${Dtype}* filter, ${Dtype}* output) {
+__global__ void __launch_bounds__(1024)
+conv2d_dw_forward_kernel(const ${Dtype}* input, const ${Dtype}* filter, ${Dtype}* output) {
   const int in_rows = ${bottom_width};
   const int in_cols = ${bottom_height};
   const int in_depth = ${channels};
@@ -145,7 +146,8 @@ const ${Dtype}* bottom_data, const ${Dtype}* weight_data, ${Dtype}* top_data) {
 
 _conv2d_depthwise_kernel_backward_grad_input = kernel_loop + '''
 extern "C"
-__global__ void conv2d_dw_backward_grad_input_kernel(const ${Dtype}* const out_backprop,
+__global__ void __launch_bounds__(1024)
+conv2d_dw_backward_grad_input_kernel(const ${Dtype}* const out_backprop,
                     const ${Dtype}* const filter, ${Dtype}* const in_backprop) {
   const int in_rows = ${bottom_width};
   const int in_cols = ${bottom_height};
@@ -244,7 +246,97 @@ __global__ void conv2d_dw_backward_grad_input_kernel(
 _conv2d_depthwise_kernel_backward_grad_weight = kernel_loop + '''
 extern "C"
 __global__ void conv2d_dw_backward_grad_weight_kernel(
-    const ${Dtype}* const top_diff, const ${Dtype}* const bottom_data, ${Dtype}* const buffer_data) {
+    const ${Dtype}* const out_backprop, const ${Dtype}* const input, ${Dtype}* const filter_backprop) {
+  const int in_rows = ${bottom_width};
+  const int in_cols = ${bottom_height};
+  const int in_depth = ${channels};
+  const int filter_rows = ${kernel_w};
+  const int filter_cols = ${kernel_h};
+  const int depth_multiplier = 1;
+  const int pad_rows = ${pad_w};
+  const int pad_cols = ${pad_h};
+  const int out_rows = ${top_width};
+  const int out_cols = ${top_height};
+  const int out_depth = ${channels};
+
+
+  CUDA_KERNEL_LOOP(thread_id, ${nthreads}) {
+    // Compute the indexes of this thread in the output.
+    const int out_c = thread_id % out_cols;
+    const int out_r = (thread_id / out_cols) % out_rows;
+    const int out_d = (thread_id / out_cols / out_rows) % out_depth;
+
+    const int b = thread_id / out_depth / out_cols / out_rows;
+    // Compute the input depth and the index of depth multiplier.
+    const int in_d = out_d / depth_multiplier;
+    const int dm = out_d % depth_multiplier;
+
+    // Decide if all input is valid, if yes, we can skip the boundary checks
+    // for each input.
+    const int in_r_start = out_r * ${stride_w} - pad_rows;
+    const int in_c_start = out_c * ${stride_h} - pad_cols;
+    const int in_r_end = in_r_start + filter_rows;
+    const int in_c_end = in_c_start + filter_cols;
+
+    const int out_backprop_offset = (b * out_depth * out_rows * out_cols) +
+                                    (out_d * out_rows * out_cols) +
+                                    (out_r * out_cols) + (out_c);
+
+    const ${Dtype} out_bp = ldg(out_backprop + out_backprop_offset);
+    if (in_r_start >= 0 && in_c_start >= 0 && in_r_end < in_rows &&
+        in_c_end < in_cols) {
+      UNROLL for (int f_r = 0; f_r < filter_rows; ++f_r) {
+        const int in_r = in_r_start + f_r;
+        // Avoid repeated computation.
+        const int input_offset_temp = (b * in_depth * in_rows * in_cols) +
+                                      (in_d * in_rows * in_cols) +
+                                      (in_r * in_cols);
+
+        UNROLL for (int f_c = 0; f_c < filter_cols; ++f_c) {
+          const int in_c = in_c_start + f_c;
+          const int input_offset = input_offset_temp + in_c;
+          ${Dtype} partial_sum = ldg(input + input_offset) * out_bp;
+          //${Dtype}* addr = filter_backprop +
+            //        (dm + depth_multiplier *
+            //                  (in_d + in_depth * (f_c + filter_cols * f_r)));
+          ${Dtype}* addr = filter_backprop + (in_d * ${kernel_w} * ${kernel_h} + f_c + filter_cols * f_r);
+          atomicAdd(addr, partial_sum);
+        }
+      }
+    } else {
+      UNROLL for (int f_r = 0; f_r < filter_rows; ++f_r) {
+        const int in_r = in_r_start + f_r;
+        // Avoid repeated computation.
+        const int input_offset_temp = (b * in_depth * in_rows * in_cols) +
+                                      (in_d * in_rows * in_cols) +
+                                      (in_r * in_cols);
+        UNROLL for (int f_c = 0; f_c < filter_cols; ++f_c) {
+          const int in_c = in_c_start + f_c;
+          const int addr_temp = filter_cols * f_r;
+
+          if (in_r >= 0 && in_r < in_rows && in_c >= 0 && in_c < in_cols) {
+            const int input_offset = input_offset_temp + in_c;
+            ${Dtype} partial_sum = ldg(input + input_offset) * out_bp;
+            //${Dtype}* addr =
+            //    filter_backprop +
+            //    (dm + depth_multiplier * (in_d + in_depth * (f_c + addr_temp)));
+            ${Dtype}* addr = filter_backprop + in_d * ${kernel_w} * ${kernel_h}  + f_c + addr_temp;
+            // Potentially many threads can add to the same address so we have
+            // to use atomic add here.
+            // TODO(jmchen): If atomic add turns out to be slow, we can:
+            // 1. allocate multiple buffers for the gradients (one for each
+            // example in a batch, for example). This can reduce the
+            // contention on the destination; 2. Have each thread compute one
+            // gradient for an element in the filters. This should work well
+            // when the input depth is big and filter size is not too small.
+            atomicAdd(addr, partial_sum);
+          }
+        }
+      }
+    }
+  }
+}
+/*
   CUDA_KERNEL_LOOP(index, ${nthreads}) {
     const int h = (index / ${top_width}) % ${top_height};
     const int w = index % ${top_width};
@@ -267,6 +359,7 @@ __global__ void conv2d_dw_backward_grad_weight_kernel(
     }
   }
 }
+*/
 '''
 
 
@@ -339,18 +432,16 @@ class Conv2dDepthwise(Function):
                   stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
 
             if self.needs_input_grad[1]:
-                weight_buffer = weight.new(channels, kernel_h, kernel_w, batch_size, output_h, output_w)
-
-                n = weight_buffer.numel()
+                n = grad_output.numel()
+                grad_weight = input.new(weight.shape).zero_()
                 opt['nthreads'] = n
 
                 f = load_kernel('conv2d_dw_backward_grad_weight_kernel',
                                 _conv2d_depthwise_kernel_backward_grad_weight, **opt)
                 f(block=(CUDA_NUM_THREADS,1,1),
                   grid=(GET_BLOCKS(n),1,1),
-                  args=[grad_output.data_ptr(), input.data_ptr(), weight_buffer.data_ptr()],
+                  args=[grad_output.data_ptr(), input.data_ptr(), grad_weight.data_ptr()],
                   stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
-                grad_weight = weight_buffer.view(weight.size() + (-1,)).sum(-1)
 
         return grad_input, grad_weight
 
